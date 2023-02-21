7a007c60ac5a77928353e9ab52dcd36afeaadb5b
hannahellis4242 <hannahellis4242@gmail.com>
2023-02-21 20:42:46 +0000

    Adding other messages for finding

--- book2/src/message/Message.ts
+++ book2/src/message/Message.ts
@@ -1,10 +1,21 @@
+import { ObjectId } from "mongodb";
 import Signatures from "../model/Signatures";
 
 export interface Create {
-  signatures: Signatures;
+  signatures?: Signatures;
 }
 
-export interface Find {}
+export interface Pages {
+  equal?: number;
+  min?: number;
+  max?: number;
+}
+
+export interface Find {
+  id?: ObjectId;
+  signatures?: Signatures;
+  pages?: Pages;
+}
 
 type Message = Create | Find;
 
--- book2/src/message/handleMessage.ts
+++ book2/src/message/handleMessage.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
-import { addBook, findBookBySignatures } from "../database/books";
+import { ObjectId } from "mongodb";
+import {
+  addBook,
+  findBookByID,
+  findBookByPages,
+  findBookBySignatures,
+} from "../database/books";
 import Book from "../model/Book";
+import Signatures from "../model/Signatures";
 import Service from "../Service";
+import { Create, Find, Pages } from "./Message";
 
-const handleCreate = (queue: Service) => async (message: any) => {
+const handleCreate = (queue: Service) => async (message: Create) => {
   const { signatures } = message;
   if (!signatures) {
     await axios.post(queue.url(), {
@@ -30,9 +38,8 @@ const handleCreate = (queue: Service) => async (message: any) => {
   console.log("existing :", existing);
   if (existing) {
     await axios.post(queue.url(), {
-      message,
       created: false,
-      reason: "already exists",
+      id: existing._id,
     });
     return;
   }
@@ -51,15 +58,76 @@ const handleCreate = (queue: Service) => async (message: any) => {
     id: bookId,
   });
 };
-const handleMessage = (queue: Service) => async (message: any) => {
-  const handleCreateFn = handleCreate(queue);
-  const { command } = message;
-  if (!command) {
-    axios.post(queue.url(), { message, error: "no command given" });
+
+const handleFindById = async (queue: Service, id: ObjectId) => {
+  const result = await findBookByID(new ObjectId(id));
+  if (result) {
+    await axios.post(queue.url(), {
+      result,
+    });
+  } else {
+    await axios.post(queue.url(), {
+      id,
+    });
+  }
+};
+
+const handleFindBySignatures = async (
+  queue: Service,
+  signatures: Signatures
+) => {
+  const result = await findBookBySignatures(signatures);
+  if (result) {
+    await axios.post(queue.url(), {
+      result,
+    });
+  } else {
+    await axios.post(queue.url(), {
+      signatures,
+    });
+  }
+};
+
+const handleFindByPages = async (queue: Service, pages: Pages) => {
+  const { equal, min, max } = pages;
+  const result = equal
+    ? await findBookByPages(equal, equal)
+    : await findBookByPages(min, max);
+  await axios.post(queue.url(), {
+    pages,
+    found: result.length,
+    result,
+  });
+};
+
+const handleFind = (queue: Service) => async (find: Find) => {
+  const { id, signatures, pages } = find;
+  if (id) {
+    await handleFindById(queue, id);
     return;
   }
-  if (command === "create") {
-    await handleCreateFn(message);
+  if (signatures) {
+    await handleFindBySignatures(queue, signatures);
+    return;
+  }
+  if (pages) {
+    await handleFindByPages(queue, pages);
+    return;
+  }
+};
+
+const handleMessage = (queue: Service) => async (message: any) => {
+  const handleCreateFn = handleCreate(queue);
+  const handleFindFn = handleFind(queue);
+  const { create } = message;
+  if (create) {
+    await handleCreateFn(create);
+    return;
+  }
+  const { find } = message;
+  if (find) {
+    await handleFindFn(find);
+    return;
   }
 };
 
