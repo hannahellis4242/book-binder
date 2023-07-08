948180f4e57a3ca34d068c75012be3b84ce3d010
hannahellis4242 <hannahellis4242@gmail.com>
2023-07-08 07:19:44 +0100

    everything so far

--- src/routes/page.ts
+++ src/routes/page.ts
@@ -10,21 +10,25 @@ page.get("/", (_, res) => res.render("page", data));
 page.post("/submit", async (req, res) => {
   const { signatures } = req.body;
   if (!signatures) {
-    res.send("Error - no signatures given");
+    res.redirect(`/page/result?error=input`);
     return;
   }
   if (!pattern.test(signatures)) {
-    res.send("Error - signatures are not a list of numbers");
+    res.redirect(`/page/result?error=numbers`);
+    return;
   }
-  const list: number[] = JSON.parse(`[${signatures}]`);
-  console.log(list);
   try {
     const response = await axios.get("http://page_sequence:8080/", {
-      params: { signatures: list },
+      params: { signatures: `[${signatures}]` },
     });
     res.json(response.data);
   } catch (e) {
     res.send(`Error - ${e}`);
   }
+});
+page.get("/result", (req, res) => {
+  const { error } = req.query;
+
+  res.render("pageResult", { ...data, error });
 });
 export default page;
