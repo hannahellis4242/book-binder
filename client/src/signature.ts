b0961cf933c7c15f7e7be3a2d20ec1bc493c6c0f
hannahellis4242 <hannahellis4242@gmail.com>
2023-07-12 12:33:49 +0100

    making sure max is never less than min plus setting some defaults

--- client/src/signature.ts
+++ client/src/signature.ts
@@ -1,2 +1,17 @@
 const min = document.getElementById("min");
-console.log(min);
+const max = document.getElementById("max");
+if (min !== null && max !== null) {
+  const minInput = min as HTMLInputElement;
+  const maxInput = max as HTMLInputElement;
+  if (minInput !== null && minInput !== null) {
+    minInput.addEventListener("change", (ev: Event) => {
+      ev.preventDefault();
+      maxInput.min = minInput.value;
+      const minNum = Number.parseInt(minInput.value);
+      const maxNum = Number.parseInt(maxInput.value);
+      if (minNum > maxNum) {
+        maxInput.value = minInput.value;
+      }
+    });
+  }
+}
