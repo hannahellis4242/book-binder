06f4ba8ab2f7c111d7507be4082cf7e841e7c639
hannahellis4242 <hannahellis4242@gmail.com>
2023-07-27 23:44:09 +0100

    Making sure at least one option is selected

--- client/src/signature.ts
+++ client/src/signature.ts
@@ -1,8 +1,6 @@
-const min = document.getElementById("min");
-const max = document.getElementById("max");
+const [min, max] = ["min", "max"].map((x) => document.getElementById(x));
 if (min !== null && max !== null) {
-  const minInput = min as HTMLInputElement;
-  const maxInput = max as HTMLInputElement;
+  const [minInput, maxInput] = [min, max].map((x) => x as HTMLInputElement);
   if (minInput !== null && minInput !== null) {
     minInput.addEventListener("change", (ev: Event) => {
       ev.preventDefault();
@@ -15,3 +13,28 @@ if (min !== null && max !== null) {
     });
   }
 }
+const options = [3, 4, 5, 6]
+  .map((x) => x.toString())
+  .map((x) => document.getElementById(x))
+  .filter((x) => x !== null)
+  .map((x) => x as HTMLInputElement)
+  .filter((x) => x !== null);
+
+const numberOfOptionsSelected = () =>
+  options
+    .map((x) => x.checked)
+    .map((x) => (x ? 1 : 0))
+    .reduce<number>((x, y) => x + y, 0);
+
+if (options.length > 0 && numberOfOptionsSelected() === 0) {
+  options[0].checked = true;
+}
+
+options.forEach((option) =>
+  option.addEventListener("change", (ev: Event) => {
+    ev.preventDefault();
+    if (numberOfOptionsSelected() === 0) {
+      options[0].checked = true;
+    }
+  })
+);
