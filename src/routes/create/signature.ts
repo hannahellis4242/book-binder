208d0a35745b2ba04bd8e43e5be4b8501f9483b3
hannahellis4242 <hannahellis4242@gmail.com>
2023-08-29 23:44:09 +0100

    wip

---
+++ src/routes/create/signature.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import PageData from "../../model/PageData";
+
+const data = new PageData("Pages in your book");
+
+const signatures = Router();
+pages.get("/", (_, res) => res.render("create/max", data));
+pages.post("/", (req, res) => {
+  const { value } = req.body;
+  if (!value) {
+    res.redirect("/error");
+    return;
+  }
+  const num = Number.parseInt(value);
+  if (Number.isNaN(num)) {
+    res.redirect("/error");
+    return;
+  }
+  const { report } = req.session;
+  if (report) {
+    report.maxAllowed = num;
+  }
+  console.log(report);
+  res.redirect("/create/signatures");
+});
+
+export default pages;
